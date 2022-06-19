b9ddfc833db6963f7c6a5ef9293e9f00795776e6
Maria Zadnepryanets <mariya.z@gmail.com>
2022-06-19 11:30:20 +0200

    Add sample data

---
+++ src/data/index.js
@@ -0,0 +1,27 @@
+export const data = [
+	{
+		"name": "Dana Robles",
+		"date": "Feb 6, 2022",
+		"company": "Aliquam Ultrices Iaculis Ltd"
+	},
+	{
+		"name": "Gloria Cobb",
+		"date": "Dec 6, 2022",
+		"company": "Nullam Scelerisque Institute"
+	},
+	{
+		"name": "Kylynn Woods",
+		"date": "Oct 13, 2021",
+		"company": "Pharetra Ut Limited"
+	},
+	{
+		"name": "Mohammad Osborn",
+		"date": "Jul 26, 2022",
+		"company": "Quisque Tincidunt Incorporated"
+	},
+	{
+		"name": "Zelenia Neal",
+		"date": "Mar 25, 2023",
+		"company": "Sed PC"
+	}
+];
